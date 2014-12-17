bb89e8639618e4e8172e47a2e26afc53ec03cfc0
Luke Deniston <luke@agrian.com>
2014-12-17 08:38:51 -0800

    More documentation updates.

--- BlargBucket/Models/Base Classes/BlargManagedObject.swift
+++ BlargBucket/Models/Base Classes/BlargManagedObject.swift
@@ -9,8 +9,16 @@
 import UIKit
 import CoreData
 
+/**
+	Main NSManagedObject subclass that all models should inherit from.
+*/
+
 @objc(BlargManagedObject)
 public class BlargManagedObject: NSManagedObject {
+
+	//TODO: Remove this
+
+	/// DEPRECATED since we moved to MagicalRecord. This will get refactored out at some point.
 	func checkAndSetDate(date: AnyObject, key: String){
 		var utc: AnyObject = date
 		if utc is NSString {
@@ -22,11 +30,16 @@ public class BlargManagedObject: NSManagedObject {
 		self.didChangeValueForKey(key)
 	}
 
+	/// Makes this available to our test host. Just calls the MagicalRecord function `importFromObject` with the same arguments.
 	public override class func importFromObject(data:AnyObject!) -> AnyObject {
 		return super.importFromObject(data)
 	}
 
-	/// Workaround for a MagicalRecord bug
+	/** 
+		Workaround for a MagicalRecord bug.
+	
+		When magical record tries to get the entity name for a Swift class, it gets the full "BlargBucket.EntityName" which later causes issues. This splits that string and only returns the "EntityName" part.
+	*/
 	class func MR_entityName() -> String{
 		return NSStringFromClass(self).componentsSeparatedByString(".").last as String!
 	}
